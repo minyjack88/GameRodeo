fd38de3ad6fd4dbd9391cd0af49f6a057f3d1469
SimonQ <Simonquade@hotmail.com>
2015-04-10 22:38:37 +0200

    Added inventory and scrap/artifact items

---
+++ Edge of Space/Assets/Scripts/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum ArtifactType {Blink, Battery, Overdrive}
+
+
+public class Inventory : MonoBehaviour {
+
+    public int scrap = 0;
+    public int money = 0;
+    public List<ArtifactType> artifacts = new List<ArtifactType>();
+    public int pickUpLayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == pickUpLayer)
+        {
+            collision.gameObject.GetComponent<PickupableItem>().PickupItem(this);
+        }
+
+    }
+
+    public void AddScrap(int amount)
+    {
+        scrap += amount;
+    }
+
+    public void AddArtifact(ArtifactType artifactType)
+    {
+        artifacts.Add(artifactType);
+    }
+}
