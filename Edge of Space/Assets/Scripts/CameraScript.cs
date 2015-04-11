ae9cb5aff663324cd8574c3aced3eb7e448092bc
Emil Vad <jozan56@gmail.com>
2015-04-11 17:29:23 +0200

    camera

---
+++ Edge of Space/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    
+    public GameObject followtarget;
+    public float maxDistance;
+    public float followDitstance;
+    public float followSpeed;
+
+
+    void Update()
+    {
+
+        if(Distance2D(followtarget.transform.position,transform.position) >= followDitstance)
+        { 
+        
+        }
+
+        if (Distance2D(followtarget.transform.position, transform.position) >= followDitstance)
+        {
+
+        }
+    }
+
+    float Distance2D(Vector3 a, Vector3 b)
+    {
+        return Mathf.Sqrt(((b.x - a.x) * (b.x - a.x) + (b.y-a.y)*(b.y-a.y)));
+    }
+}
