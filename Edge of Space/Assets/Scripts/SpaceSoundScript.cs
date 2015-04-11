76c4c8787f2da727ab4909a1196e7e23853a52a5
Emil Vad <jozan56@gmail.com>
2015-04-11 13:13:23 +0200

    SoundStuff

---
+++ Edge of Space/Assets/Scripts/SpaceSoundScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpaceSoundScript : MonoBehaviour {
+
+    public GameObject SoundSoucre;
+    public float rottaionSpeed = 2;
+    public AudioClip[] ClipToUse;
+
+    public float maxApperInterval = 10;
+    public float minApperInterval = 5;
+
+    private float ApperensTimer;
+    private AudioSource aSource;
+
+    void Start()
+    {
+        aSource = SoundSoucre.GetComponent<AudioSource>();
+        ApperensTimer = Random.Range(minApperInterval,maxApperInterval);
+    }
+
+
+    void Update()
+    {
+        if (ApperensTimer >= 0 && !aSource.isPlaying)
+        {
+            AudioClip clip = ClipToUse[Random.Range(0, ClipToUse.Length)];
+            if (aSource.clip != clip)
+            {
+                aSource.clip = clip;
+                aSource.Play();
+                ApperensTimer = Random.Range(minApperInterval, maxApperInterval);
+            }
+        }
+        else if(!aSource.isPlaying)
+            ApperensTimer -= Time.deltaTime;
+
+
+        transform.RotateAround(Vector3.zero, Vector3.forward, rottaionSpeed * Time.deltaTime);
+
+    }
+
+}
