d670d9f1a81e6c885aa60c33be8bd595ae9a11a6
Emil Vad <jozan56@gmail.com>
2015-04-12 00:47:53 +0200

    Thruster Add

---
+++ Edge of Space/Assets/Scripts/Thruster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thruster : MonoBehaviour 
+{
+    public ParticleSystem ps;
+    public KeyCode activate;
+
+    void Update()
+    {
+        if (Input.GetKey(activate))
+        {
+            if (ps.isStopped)
+                ps.Play();
+        }
+        else
+        {
+            if (ps.isPlaying)
+                ps.Stop();
+        }
+
+    }
+
+}
